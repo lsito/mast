4c766a7fc3668ddf245c10e3b8642e38db6bdb17
lsito <leonardo.sito@cern.ch>
2026-03-31 15:50:15 +0200

    initializing

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mast",  # Name of your package
+    version="0.1.0",  # Initial release version
+    author="Leonardo Sito",  # Your name
+    author_email="leonardo.sito@cern.com",  # Your email
+    description="A package multicell accelerating structures (mast) tuning",  # Short description
+    long_description=open("README.md").read(),  # Optional, long description from README
+    long_description_content_type="text/markdown",  # If README is in Markdown format
+    url="https://github.com/lsito/mast.git",  # URL to the package repo (GitHub)
+    packages=find_packages(),  # Automatically find all packages in this directory
+    install_requires=[  # List any package dependencies
+        "numpy", 
+        "pandas",
+        # Add other dependencies here if needed
+    ],
+    classifiers=[  # Optional: Classifiers to describe the project
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Specify the Python version requirement
+)
